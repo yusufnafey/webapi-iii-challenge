84ab2c027b55178cbece8a99fe611cc8a2e3ef4f
Yusuf Nafey <yusufnafey@yahoo.com>
2019-08-07 15:01:04 -0700

    finished logger middleware

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
-const express = 'express';
+const express = "express";
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
+  let date = new Date();
+  let timestamp = date.getTime();
+  const method = req.method;
+  const url = req.url;
+  console.log(`You made a ${method} request to ${url} on ${timestamp}.`);
+}
 
-};
+function validateUserId(req, res, next) {}
+
+function validateUser(req, res, next) {}
+
+function validatePost(req, res, next) {}
 
 module.exports = server;
